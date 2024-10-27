9035e5155558481e3f9b39b8beb2dad0c0b302d0
print-alan <a.montiel.work@gmail.com>
2024-10-27 12:14:37 -0600

    Avance #01-python

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/Print-Alan.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/Print-Alan.py
@@ -1,26 +1,65 @@
-"""Operadores y estructuras de control"""
+    #Operadores y estructuras de control
 
-#Operadores aritméticos
+ #Operadores aritméticos
 
-a = 3
-b = 5
-suma = a + b
-resta = a - b
-multiplicacion = a * b
-Division = a / b
-Modulo = a % b
-Exponenciacion = a ** b
-Divicion_entera = a // b
+#Suma
+print(f"suma: 10 + 3 = {10 + 3}")
 
-#Operadores logicos
+#resta
+print(f"resta: 10 - 3 = {10 - 3}")
 
-#Operadores de comparación
+#Multiplicacion
+print(f"Multiplicacion: 10 * 3 = {10 * 3}")
 
-#Operadores de asignación
+#Divicion
+print(f"divicion: 10 / 3 = {10 / 3}")
 
-#Operadores de identidad
+#Modulo
+print(f"modulo: 10 % 3 = {10 % 3}")
 
-#Operadores de pertenencia
+#Exponente
+print(f"Exponente: 10 ** 3 = {10 ** 3}")
 
-#Operadores de bits
+#Divicion entera
+print(f"Divicion entera: 10 // 3 = {10 // 3}")
 
+
+ #Operadores de comparación
+
+#Igualdad
+print(f"10 == 3 {10 == 3}") # ¿10 es igual a 3? : False
+
+#Desigualdad 
+print(f"10 != 10 {10 != 3}") # ¿10 es distinto de a 3? : True
+
+#Mayor que
+print(f"10 > 3 {10 > 3}") # ¿10 Mayor que 3? : True
+
+#Menor que
+print(f"10 < 3 {10 < 3}") # ¿10 manor que 3? : False
+
+#Mayor o igual que
+print(f"10 >= 3 {10 >= 3}") # ¿10 mayor que 3? : True
+
+#Menor o igual que
+print(f"10 <= 3 {10 <= 3}") # ¿10 menor o igual a 3? : false
+
+
+ #Operadores logicos
+
+#And lógico
+print(f"10 + 3 == 13 and 5 - 1 == 4 es {10 + 3 == 13 and 5 - 1 == 4}") #Las dos operaciones deben ser correctas para el true
+
+#Or lógico
+print(f"10 + 3 == 12 or 5 - 1 == 4 es {10 + 3 == 12 or 5 - 1 == 4}") #Una operación debe ser correcta para el true
+
+#Not lógico
+print(f"not 10 + 3 == 14 es {not 10 + 3 == 14}")#
+
+ #Operadores de asignación
+
+ #Operadores de identidad
+
+ #Operadores de pertenencia
+
+ #Operadores de bits
